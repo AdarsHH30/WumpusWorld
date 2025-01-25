353f532474b7a4b2fa612170dee16f04d9767e4e
AdarsHH30 <adarshhegde380@gmail.com>
2025-01-25 14:55:42 +0530

    Update world.py with improved placement logic and add win.txt; modify .gitignore

--- game.py
+++ game.py
@@ -1,107 +1,173 @@
-
-#map will be displayed by hiding all the boxed except (0,0)
-#options will be given to the user to move left, right, up, down 
-#if based on the user movement the state of the box will be displayed
-#when the user enters start then the game will be started
-# user enters his move 
-from world import *
+# map will be displayed by hiding all the boxed except (0,0)
+# options will be given to the user to move left, right, up, down
+# if based on the user movement the state of the box will be displayed
+# when the user enters start then the game will be started
+# user enters his move
 # from types import List
+from world import *
+import subprocess
+
 
 class Game:
-    
-    def __init__(self):
-        self.map=[["####" for _ in range(worldSize)] for _ in range(worldSize)]
-        self.map[0][0]=""
-       
-    def endGameMessage(self,message):
-        print(message)
-    
-    
-            
-    def listPossibleMove(self,row:int,col:int):
-        arr=[]
-        
-        if row+1 < worldSize:
-            arr.append("S")
-        if row-1 >=0:
-            arr.append("W")
-        if col+1 < worldSize:
-            arr.append("D")
-        if col-1 >= 0 :
-            arr.append("A")
+    UP, DOWN, RIGHT, LEFT = "UP(W)", "DOWN(S)", "RIGHT(D)", "LEFT(A)"
+    FIRE, MOVE, QUIT = "FIRE(F)", "MOVE(M)", "QUIT(Q)"
 
-        return arr
-    
+    def __init__(self):
+        self.map = [["𝍈𝍈𝍈" for _ in range(worldSize)] for _ in range(worldSize)]
+        self.map[0][0] = ""
 
     def instructions(self):
-        print("\nDIRECTIONS:")
-        print("\n W - > UP \n S -> DOWN \n A -> LEFT \n D-> RIGHT\n")
-        print("SIGNs:\n")
-        print(f"Gold -> {GLITTER}\t Wumpus -> {WUMPUS} \nStench -> {STENCH} \t breeze -> {BREEZE} \nPit -> {PIT}")
 
+        DIRECTION = "<< DIRECTION >>"
+        CONTROLS = "<< CONTROLS >>"
+        directions = f"{DIRECTION:>10}=>{self.DOWN:>20}{self.UP:>20}{self.RIGHT:>20}{self.LEFT:>20}"
+        controls = f"{CONTROLS:>10}=>{self.MOVE:>30}{self.FIRE:>30}{self.QUIT:>30}"
+        print(directions, "\n\n", controls)
 
-    def updateRowCOl(self,row:int,col:int,move:str):
-        if move == "W":
-            return row-1,col
-        elif move == "S":
-            return row+1,col
-        elif move == "D":
-            return row,col+1 
-        elif move == "A":
-            return row,col-1
-        
+    def endGameMessage(self, message):
+        print(message)
 
+    def listPossibleMove(self, row: int, col: int):
+        arr = []
 
-    def winORloose(self,currentVal):
+        if row + 1 < worldSize:
+            arr.append("S")
+        if row - 1 >= 0:
+            arr.append("W")
+        if col + 1 < worldSize:
+            arr.append("D")
+        if col - 1 >= 0:
+            arr.append("A")
+        return arr
+
+    # def instructions(self):
+    #     print("\nDIRECTIONS:")
+    #     print("\n W - > UP \n S -> DOWN \n A -> LEFT \n D-> RIGHT\n")
+    #     print("SIGNs:\n")
+    #     print(
+    #         f"Gold -> {GLITTER}\t Wumpus -> {WUMPUS} \nStench -> {STENCH} \t breeze -> {BREEZE} \nPit -> {PIT}"
+    #     )
+
+    def winORloose(self, currentVal):
         if PIT in currentVal:
             self.endGameMessage("AGENT IS KILLED BY FALLING INTO A PIT...:(")
 
         elif WUMPUS in currentVal:
             self.endGameMessage("AGENT IS KILLED BY WUMPUS...:(")
-                
+
         elif GLITTER in currentVal:
+            subprocess.run("clear")
+            f = open("win.txt", "r")
+            print(f.read())
             self.endGameMessage("AGENT WON BY ACQUIRING GOLD .. :O")
-        
-        else:return False
+            exit()
 
-    def printMap(self):
-        print("|"+" "*(worldSize-1),end="")
-        
+        else:
+            return False
+
+    def padding(self):
+        padding = f"{"":<50}"
+        print(padding, end="")
+
+    def printMap(self, row, col):
+        print(self.padding(), "*", end="")
         for i in range(worldSize):
-            for j in range(worldSize): 
-                print(self.map[i][j],end="  |  ")
+            print("-" * 3 + "*", end="")
+        print("\n|", end="")
+        for i in range(worldSize):
+            self.padding()
+            for j in range(worldSize):
+                if i == row and j == col:
+                    print(agent, end=" |")
+                else:
+                    print(self.map[i][j], end=" |")
             print("\n")
-            # for x in range(worldSize):
-            #     print("__"*worldSize,end="")
-            print("\n")
-            print("|   ",end="")
-    
 
-    def agentMove(self,row:int,col:int):
+    def displayMoves(self, moves):
+        print("\n   ")
+        for s in moves:
+            padded = f"{s:>50}"
+            print(padded, end="")
+        print("\n")
 
-        while (True):
-            currentVal=cave[row][col]
+    def displayControls(self):
+        CONTROLS = "<< CONTROLS >>"
+        controls = f"{CONTROLS:>10}=>{self.MOVE:>30}{self.FIRE:>30}{self.QUIT:>30}"
+        print(controls)
+
+    def readyTOfire(self, row, col):
+        moves = self.listPossibleMove(row, col)
+        self.displayMoves(moves)
+        dir = f"{"ENTER  THE DIRECTION TO FIRE >":>75}"
+        if dir.upper() not in moves:
+            self.printMap(row, col)
+            print("INVALID MOVE")
+
+    def updateRowCOl(self, row: int, col: int, move: str):
+        match move:
+            case "W":
+                return row - 1, col
+            case "S":
+                return row + 1, col
+            case "D":
+                return row, col + 1
+            case "A":
+                return row, col - 1
+
+    def readyToMove(self, row, col):
+        moves = self.listPossibleMove(row, col)
+        self.displayMoves(moves)
+        dir = f"{"ENTER  THE DIRECTION TO MOVE >":>75}"
+        agent_move = input(dir)
+        print("Agent move is :", agent_move)
+        if agent_move.upper() not in moves:
+            self.printMap(row, col)
+            print("INVALID MOVE")
+            return row, col
+        r, c = self.updateRowCOl(row, col, agent_move.upper())
+        self.map[r][c] = cave[r][c]
+        return r, c
+
+    def effectDisplay(self, currentVal):
+        padding = f"{"":<50}"
+        print(padding, "---------------------------------------")
+        print(padding, f"{"CURRENT LOCATION EFFECTS":<10}:", end="")
+        if currentVal == "":
+            print("Noting")
+        else:
+            print(currentVal)
+        print(padding, "---------------------------------------")
+
+    def agentMove(self, row: int, col: int):
+        while True:
+            currentVal = cave[row][col]
+            self.effectDisplay(currentVal)
             if self.winORloose(currentVal):
                 exit
- 
-            moves=self.listPossibleMove(row,col)
-            
-            for move in moves:
-                print(move)
-            agent_move=input("Enter your move::")
-            if agent_move.upper() not in moves:
-                print("Invalid Move")
-                continue
-            row,col=self.updateRowCOl(row,col,agent_move.upper())
-            print(row,col)
-            self.map[row][col]=cave[row][col]
-            self.printMap()
+            self.displayControls()
+            ctrl = f"{"ENTER  YOUR CONTROL =":>75}"
+            agent_move = input(ctrl)
+            if agent_move.upper() == "F":
+                self.readyTOfire(row, col)
+            elif agent_move.upper() == "M":
+                row, col = self.readyToMove(row, col)
+            elif agent_move.upper() == "Q":
+                obj.printGrid()
+                break
+            else:
+                print("INVALID MOVE TRY again")
+            subprocess.run("clear")
+            self.printMap(row, col)
 
-if __name__=="__main__":
-    obj=World()
-    cave,worldSize=obj.returnMap()
-    gameObj=Game()
-    gameObj.instructions()
-    gameObj.printMap()  
-    gameObj.agentMove(0,0)
-    
+    def displayGame(self):
+        self.instructions()
+        self.printMap(0, 0)
+        self.agentMove(0, 0)
+
+
+if __name__ == "__main__":
+    obj = World()
+    cave, worldSize = obj.returnMap()
+    obj.resizeMap()
+    gameObj = Game()
+    gameObj.displayGame()
--- world.py
+++ world.py
@@ -1,77 +1,103 @@
 import random as Random
-#gridSize*gridSize grid which is world 
-#0,0 where the Agent will start the game
-#one 2 random number will be generated from 1 to gridSize which is the location of the wumpus then from there the surrounding will the the Stench
-#shoot,move
+
+# gridSize*gridSize grid which is world
+# 0,0 where the Agent will start the game
+# one 2 random number will be generated from 1 to gridSize which is the location of the wumpus then from there the surrounding will the the Stench
+# shoot,move
 
 
-WUMPUS="wumpus"
-PIT="pit"
-STENCH="stench"
-BREEZE="breeze"
-GLITTER="glitter"
+WUMPUS = "👻"
+PIT = "🕳️"
+STENCH = "💨"
+BREEZE = "༄"
+GLITTER = "💰"
+agent = "🤖"
+
 
 class World:
 
-    # gridSize=int(input("Enter World Size :")) 
-    gridSize=4 
+    gridSize = 4
 
     def __init__(self):
         self.world = [["" for _ in range(self.gridSize)] for _ in range(self.gridSize)]
-    
-    def place(self,row:int,col:int,effect:str):
-        if row-1  >= 0 :        
-            self.world[row-1][col]+=effect
-        if row+1 < self.gridSize:
-            self.world[row+1][col]+=effect
-        if col+1 < self.gridSize:
-            self.world[row][col+1]+=effect
-        if col-1 >= 0:
-            self.world[row][col-1]+=effect
-    
-    def pitSpawn(self):
-        noPit=self.gridSize
-        for i in range(noPit):
-            row=Random.randrange(1,self.gridSize)
-            col=Random.randrange(1,self.gridSize)
-            if self.world[row][col]!=WUMPUS and self.world[row][col]!=GLITTER and self.world[row][col]!=PIT:
-                self.world[row][col]=PIT
-                self.place(row,col,BREEZE)
-            i-=1
-        
-    def goldSpawn(self):
-        row=Random.randrange(1,self.gridSize)
-        col=Random.randrange(1,self.gridSize)
-        if self.world[row][col]!=WUMPUS and self.world[row][col]!=PIT:
-            self.world[row][col]=GLITTER
-        self.pitSpawn()
-    
-    def wumpusSpawn(self):
-        wumpusCount=1
-        while(wumpusCount):
-            row=Random.randrange(1,self.gridSize)
-            col=Random.randrange(1,self.gridSize)
-            if (row!=1 or col!=0 ) and (row!=0 or col!=1):
-                self.world[row][col]=WUMPUS
-                wumpusCount-=1
 
-        self.place(row,col,STENCH)
+    def canPlace(self, check, effect, val):
+        if effect not in val and val not in check:
+            return True
+        return False
+
+    def place(self, row: int, col: int, effect: str):
+        check = [WUMPUS, PIT]
+
+        if row - 1 >= 0:
+            if self.canPlace(check, effect, self.world[row - 1][col]):
+                self.world[row - 1][col] += effect
+
+        if row + 1 < self.gridSize:
+            if self.canPlace(check, effect, self.world[row + 1][col]):
+                self.world[row + 1][col] += effect
+
+        if col + 1 < self.gridSize:
+            if self.canPlace(check, effect, self.world[row][col + 1]):
+                self.world[row][col + 1] += effect
+        if col - 1 >= 0:
+            if self.canPlace(check, effect, self.world[row][col - 1]):
+                self.world[row][col - 1] += effect
+
+    def pitSpawn(self):
+        noPit = self.gridSize
+        for i in range(noPit):
+            row = Random.randrange(1, self.gridSize)
+            col = Random.randrange(1, self.gridSize)
+            if (
+                WUMPUS not in self.world[row][col]
+                and GLITTER not in self.world[row][col]
+                and PIT not in self.world[row][col]
+            ):
+                self.world[row][col] = PIT
+                self.place(row, col, BREEZE)
+            i -= 1
+
+    def goldSpawn(self):
+        while True:
+            row = Random.randrange(1, self.gridSize)
+            col = Random.randrange(1, self.gridSize)
+            if WUMPUS not in self.world[row][col]:
+                print(row, col)
+                self.world[row][col] = GLITTER
+                break
+        self.pitSpawn()
+
+    def wumpusSpawn(self):
+        row = Random.randrange(1, self.gridSize)
+        col = Random.randrange(1, self.gridSize)
+        self.world[row][col] = WUMPUS
+        self.place(row, col, STENCH)
         self.goldSpawn()
 
     def returnMap(self):
         self.wumpusSpawn()
         # var=self.world
         # # print(var)
-        return self.world,self.gridSize
-    
+        return self.world, self.gridSize
+
+    def resizeMap(self):
+        for i in range(self.gridSize):
+            for j in range(self.gridSize):
+                if len(self.world[i][j]) == 2:
+                    self.world[i][j] += " "
+                elif len(self.world[i][j]) == 1:
+                    self.world[i][j] = " " + self.world[i][j] + " "
+                elif not len(self.world[i][j]):
+                    self.world[i][j] = "  "
+
     def printGrid(self):
+        self.resizeMap()
         for i in self.world:
             print(i)
-   
 
-# if __name__=="__main__":
-#     obj=World()
-#     val=obj.returnMap()
-#     obj.printGrid()
 
-    
+if __name__ == "__main__":
+    obj = World()
+    obj.wumpusSpawn()
+    obj.printGrid()
